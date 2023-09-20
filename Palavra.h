7375af60ca3051365aab1462672f68d28eb5443c
naiynull <manuborges011@gmail.com>
2023-09-20 16:54:46 -0300

    d

--- Palavra.h
+++ Palavra.h
@@ -9,8 +9,8 @@ class Palavra
     public:
         Palavra();
         //comparar duas strings e determinara o numero de letras que são comuns entre as duas e transformarar as letras comuns e uma nova string
-       std::string LetrasCompartilhadas( std::string, std::string);
        std::string SortearPalavra(const std::ifstream&); 
+     std::string Selecionadas(const std::string &, int);
     private:
     std::string palavra;
     std::string letrasReveladas;
--- main.cpp
+++ main.cpp
@@ -5,11 +5,19 @@
 #include "Palavra.h"
 
 using namespace std;
+void Menu(){
+    system("clear");
+    cout<<"Escolha o nível de dificuldade:\n";
+    cout<<"1.facil\n";
+    cout<<"2.Medío\n";
+    cout<<"3.Difícil\n";
+}
 
 int main()
 {
+    Menu();
  Palavra P;
-   
+   int i;
     // ler todas as palavras em um array
     const int Tam = 1471; // quantidade de palavras presente no arquivo
 
@@ -30,14 +38,29 @@ int main()
  
     }
 
-    std::string palavraSorteada =P.SortearPalavra(arq);
+    std::string palavraSorteada =P.SortearPalavra(arq);// palavra sorteada recebe o arquivo ou seja o endereço de memoria do meu arquivo que esta na função SortearPalavra
 
-    if(!palavraSorteada.empty()) {
-        std::cout << "Palavra sorteada: " << palavraSorteada << std::endl;
-    }else{
-        std::cerr << "Nenhuma palavra disponível." << std::endl;
+    if(!palavraSorteada.empty()) {// se não estiver vazia 
+        cout << "Palavra sorteada: " << palavraSorteada << endl;// vai mostrar a palavra sorteada 
+
+        // Print each letter vertically
+        for (size_t i = 0; i < palavraSorteada.length(); i++) {// para representar tamanhos de objetos ou índices de arrays.
+        // ele só pode representar valores não negativos
+        //A variável i é declarada como size_t para ser usada como um índice para percorrer cada caractere da string palavraSorteada. 
+        //o método length() de uma string retorna um valor do tipo size_t,tornando mais apropriado para comparações e iterações relacionadas ao tamanho de strings.
+            cout << palavraSorteada[i] << endl;
+        }
+      
+    std::string palavraSelecionada =P.Selecionadas(palavraSorteada, i);
+            std::cout << "Palavra com letra no índice " << i << ": " << palavraSelecionada << std::endl;
+        
+        }else {
+        cerr << "Nenhuma palavra disponível." << endl;
     }
 
     arq.close();
     return 0;
 }
+
+
+
