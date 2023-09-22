406f88afdfe46d0c865787be9f590d737462225b
naiynull <manuborges011@gmail.com>
2023-09-22 08:46:19 -0300

    e

--- main.cpp
+++ main.cpp
@@ -1,66 +1,54 @@
 #include <iostream>
-#include <string>
 #include <fstream>
-#include <vector>
-#include "Palavra.h"
+#include <string>
+#include <ctime>
+#include <cstdlib>
 
-using namespace std;
-void Menu(){
-    system("clear");
-    cout<<"Escolha o nível de dificuldade:\n";
-    cout<<"1.facil\n";
-    cout<<"2.Medío\n";
-    cout<<"3.Difícil\n";
-}
+void GerarPalavra(const char* Palavra) {
+    const int numPalavras = 10;
+    std::string selecionadas[numPalavras];
 
-int main()
-{
-    Menu();
- Palavra P;
-   int i;
-    // ler todas as palavras em um array
-    const int Tam = 1471; // quantidade de palavras presente no arquivo
+    std::ifstream arq(Palavra);
 
-
-
-
-   // ifstream arq;
-    //arq.open("Palavra.txt");
-
-   // string let[Tam];
-    // vamos manter esse vector é tipo uma matriz de strings (estrutura 2d)
-   // vector<vector<string>> crowssword;
-    // interar um vetor em outro vetor
-  ifstream arq("Palavras.txt");
- if(!arq.is_open()) {
-    cerr << "Erro ao abrir o arquivo de palavras." <<endl;
-        
- 
+    if (!arq.is_open()) {
+        std::cerr << "Erro ao abrir o arquivo." << std::endl;
+        return;
     }
 
-    std::string palavraSorteada =P.SortearPalavra(arq);// palavra sorteada recebe o arquivo ou seja o endereço de memoria do meu arquivo que esta na função SortearPalavra
+    // Seed para geração de números aleatórios
+    std::srand(std::time(nullptr));
 
-    if(!palavraSorteada.empty()) {// se não estiver vazia 
-        cout << "Palavra sorteada: " << palavraSorteada << endl;// vai mostrar a palavra sorteada 
+    std::string Palavras;
+    int Total = 0;
 
-        // Print each letter vertically
-        for (size_t i = 0; i < palavraSorteada.length(); i++) {// para representar tamanhos de objetos ou índices de arrays.
-        // ele só pode representar valores não negativos
-        //A variável i é declarada como size_t para ser usada como um índice para percorrer cada caractere da string palavraSorteada. 
-        //o método length() de uma string retorna um valor do tipo size_t,tornando mais apropriado para comparações e iterações relacionadas ao tamanho de strings.
-            cout << palavraSorteada[i] << endl;
+    // Ler todas as palavras do arquivo
+    while (arq >> Palavras) {
+        Total++;
+
+        // Se ainda não selecionamos 10 palavras aleatórias, escolha esta palavra
+        if (Total <= numPalavras) {
+            selecionadas[Total - 1] = Palavras;
+        } else {
+            // Caso contrário, substitua uma das palavras aleatórias já escolhidas com probabilidade 10/Total
+            int let = std::rand() % Total;
+            if (let < numPalavras) {
+                selecionadas[let] = Palavras;
+            }
         }
-      
-    std::string palavraSelecionada =P.Selecionadas(palavraSorteada, i);
-            std::cout << "Palavra com letra no índice " << i << ": " << palavraSelecionada << std::endl;
-        
-        }else {
-        cerr << "Nenhuma palavra disponível." << endl;
     }
 
     arq.close();
-    return 0;
+
+    // Imprimir as 10 palavras selecionadas aleatoriamente
+    for (int i = 0; i < numPalavras; i++) {
+        std::cout << selecionadas[i] << std::endl;
+    }
 }
 
+string PalavraCentral()
 
-
+int main() {
+    const char* Palavra = "Palavras.txt";
+    GerarPalavra(Palavra);
+    return 0;
+}
