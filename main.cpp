8e9f53345b07ab3dc3a0e5b58afe95f8e3a7a6c9
naiynull <manuborges011@gmail.com>
2023-09-27 12:29:40 -0300

    g

--- main.cpp
+++ main.cpp
@@ -7,7 +7,8 @@
 class Palavra {
 private:
     std::string selecionada;
-    std::string Palavras;
+    std::string palavras[10]; // Matriz de 10 linhas
+    char grade[10][50]; // Matriz de 10 linhas e 50 colunas
 
 public:
     const char *wordcenter;
@@ -16,12 +17,13 @@ public:
         return selecionada;
     }
 
-    std::string GetPalavras() {
-        return Palavras;
+    std::string GetPalavras(int index) {
+        return palavras[index];
     }
 
     std::string Centralpala(const char *wordcenter) {
         const int numPalavras = 1471;
+        std::string Palavras; // Declare a variável para armazenar as palavras
 
         std::ifstream arq(wordcenter);
 
@@ -58,49 +60,68 @@ public:
         }
         return selecionada;
     }
- void GerarPalavra(const char* word) {
-        const int numPalavras = 10;
-        std::ifstream arq(word);
-        if (!arq.is_open()) {
-            std::cerr << "Erro ao abrir o arquivo." << std::endl;
-            return;
-        }
 
-        std::string selecionadas[numPalavras];
+    void sortearpalavra(const char *wordcenter) {
+        Centralpala(wordcenter); // Chama Centralpala para selecionar a palavra
+        std::string palavraSelecionada = GetSeleciona();
+        while (!palavraSelecionada.empty()) {
         int Total = 0;
+        int palavrassort = 0;
+        int j=0;
+        std::string pala[10];
+        std::ifstream arq(word);
 
         // Ler todas as palavras do arquivo
-        while (arq >> Palavras) {
+        if (arq >> Palavras) {
             Total++;
-        
-            // Se ainda não selecionamos 10 palavras aleatórias, escolha esta palavra
-            if (Total <= numPalavras) {
-                selecionadas[Total - 1] = Palavras;
-            } else {
-                // Com probabilidade 10/Total, substitua uma das palavras aleatórias já escolhidas
-                int let = rand() % Total;
-                if (let < numPalavras) {
-                    selecionadas[let] = Palavras;
-                }
+            // Verificar se a palavra tem 10 caracteres
+            if (Palavras[i] == selecionada[j]) {
+                palavrassort++;
+                j++;
             }
+                i++;
+
+                // Com probabilidade 1/palavrasDe10Caracteres, escolher esta palavra
+                if (rand() % palavrassort == 0) {
+                    pala = Palavras;
+                }
         }
 
-        arq.close();
-
-        // Imprimir as 10 palavras selecionadas aleatoriamente
-        for (int i = 0; i < numPalavras; i++) {
-            std::cout << selecionadas[i] << std::endl;
+        } else {
+            std::cout << "Nenhuma palavra com 10 caracteres encontrada em sortearpalavra." << std::endl;
         }
     }
 
-    void sortearpalavra(const char *wordcenter) {
-        std::string palavraprincipal = Centralpala(wordcenter); // Chama Centralpala para selecionar a palavra
-       std::string palavraSelecionada = GetSeleciona();
-        if (!palavraSelecionada.empty()) {
-            // Imprimir a palavra selecionada aleatoriamente com 10 caracteres
-            std::cout << "Palavra selecionada em sortearpalavra: " << palavraSelecionada << std::endl;
-        } else {
-            std::cout << "Nenhuma palavra com 10 caracteres encontrada em sortearpalavra." << std::endl;
+  void PreencherMatriz() {
+    // Verifique se a palavra selecionada tem exatamente 10 caracteres
+    if (selecionada.length() != 10) {
+        std::cerr << "A palavra selecionada não tem 10 caracteres." << std::endl;
+        return;
+    }
+
+    // Encontre o ponto de partida para a vertical no centro da matriz (linha 5)
+    int inicioVertical = 5 - selecionada.length() / 2;
+
+    // Preencha a matriz com a palavra "centralpala" na vertical
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 50; j++) {
+            if (j == 25 && i >= inicioVertical && i < inicioVertical + selecionada.length()) {
+                grade[i][j] = selecionada[i - inicioVertical];
+            } else {
+                grade[i][j] = '_'; // Preenche com '.' fora da vertical
+            }
+        }
+    }
+}
+
+
+    void ExibirMatriz() {
+        std::cout << "Caça-Palavras:" << std::endl;
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 50; j++) {
+                std::cout << grade[i][j] << ' ';
+            }
+            std::cout << std::endl;
         }
     }
 };
@@ -108,8 +129,10 @@ public:
 int main() {
     srand(time(NULL));
     const char* word = "Palavras.txt";
+    std::cout<<"oi";
     Palavra P;
-    P.GerarPalavra(word);
     P.sortearpalavra(word);
+    P.PreencherMatriz();
+    P.ExibirMatriz();
     return 0;
 }
